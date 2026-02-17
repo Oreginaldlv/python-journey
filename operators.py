0303667a5e28b8074257128dcbd71e576b138186
Oreginaldlv <121742120+Oreginaldlv@users.noreply.github.com>
2026-02-17 15:12:56 -0800

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,26 @@
+income = 70000
+expenses = 32000
+
+profit = income - expenses
+monthly_income = income / 12
+credit_score = 720
+salary = 70000
+rent = 1500
+car = 450
+food = 600
+
+total_monthly_expenses = rent + car + food
+remaining_money = monthly_income - total_monthly_expenses
+
+print(f"Total Expenses: ${total_monthly_expenses}")
+print(f"Remaining Money: ${remaining_money}")
+
+print(credit_score > 700 and salary > 50000)
+print(credit_score > 750 or salary > 80000)
+
+print(f"Profit: ${profit}")
+print(f"Monthly Income: ${monthly_income}")
+
+print(income > expenses)
+print(income < expenses)
+print(income == expenses)
