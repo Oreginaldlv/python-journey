05bcef49450ffb68e34cfe12a2156d1285ccf111
Oreginaldlv <121742120+Oreginaldlv@users.noreply.github.com>
2026-02-17 14:57:55 -0800

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,15 @@
+name = "Reggie"
+age = 54
+income_goal = 70000
+pi_value = 3.14
+career = "Python Developer / Automation / Scripting"
+years_to_mastery = .082
+daily_hours = 3
+monthly_goal = income_goal / 12
+weekly_goal = income_goal / 52
+
+print(f"Monthly Goal: ${monthly_goal}")
+print(f"Weekly Goal: ${weekly_goal}")
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"My income goal is ${income_goal}")
