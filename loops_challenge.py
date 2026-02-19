7ebecdb73c7d26c9c9a2cb4a48c367672e41e277
Oreginaldlv <121742120+Oreginaldlv@users.noreply.github.com>
2026-02-19 10:28:21 -0800

    Add files via upload

--- loops_challenge.py
+++ loops_challenge.py
@@ -4,4 +4,10 @@ for salary in salaries:
     if salary > 60000:
         print(f"{salary} is greater than $60,000")
 
+"""Simple countdown timer using a Python loop."""
+
+for seconds in range(10, -1, -1):
+    print(seconds)
+
+print("Time's up!")
        
